d41636128f86decd16786636c4f8b5b0137335ee
rasmussalling <rasmussalling02@gmail.com>
2023-01-05 13:59:13 +0100

    Regler work with multi and update ReglerDriver

--- Reversi.V1/src/ReglerDriver.java
+++ Reversi.V1/src/ReglerDriver.java
@@ -2,12 +2,38 @@ public class ReglerDriver {
     public static void main(String[] args) {
         Regler hej = new Regler(7);
 
-        System.out.println(hej.startmoves(4, 6, 3)+" = false");
-        System.out.println(hej.startmoves(1, 6, 3)+" = false");
-        System.out.println(hej.startmoves(0, 4, 3)+" = false");
+        // System.out.println(hej.startmoves(4, 6, 3)+" = false");
+        // System.out.println(hej.startmoves(1, 6, 3)+" = false");
+        // System.out.println(hej.startmoves(0, 4, 3)+" = false");
 
-        System.out.println(hej.startmoves(1, 4, 3));
-        System.out.println(hej.startmoves(1, 3, 3));
+        System.out.println(hej.startmoves(1, 3, 3)+ " = true");
+        System.out.println(hej.startmoves(1, 4, 3)+ " = true");
+        System.out.println(hej.startmoves(2, 4, 4)+ " = true");
+        System.out.println(hej.startmoves(2, 3, 4)+ " = true");
+
+        hej.standardmove(1, 3, 2);
+        hej.standardmove(2, 2, 4);
+        hej.standardmove(1, 2, 3);
+        hej.standardmove(1, 5, 4);
+        hej.standardmove(1, 6, 4);
+        // System.out.println(hej.startmoves(1, 4, 3)+ " = false");
+        // System.out.println(hej.startmoves(1, 3, 3)+ " = false");
+        int[][] brat = hej.gameboard();
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+
+                System.out.print(brat[j][i] + " ");
+            }
+            System.out.println();
+        }
+       System.out.println(hej.legalmove(2));
+
+        int f = 3;
+        int g = -1;
+        f += g;
+        System.out.println(f);
+    
+
         
     }
 }
