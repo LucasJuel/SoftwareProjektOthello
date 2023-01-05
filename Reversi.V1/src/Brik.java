a55bd4523a121866d4da5013d1046327e96d4604
LucasJuel <lucas.juel.sjostrom@gmail.com>
2023-01-05 12:16:27 +0100

    add grid, text, icon, and img

--- Reversi.V1/src/Brik.java
+++ Reversi.V1/src/Brik.java
@@ -1,11 +1,16 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.ArrayList;
+import java.awt.Point;
+
 
 public class Brik extends Application{
     public static void main(String[] args) {
@@ -14,38 +19,78 @@ public class Brik extends Application{
 
         
     Group root = new Group();
-    int color = 0;
+    int color = 1;
     ArrayList<Circle> circles = new ArrayList<Circle>();
+    Text player = new Text();
 
+    
     @Override
     public void start(Stage primStage){
-        primStage.setTitle("Du lugter");
-        Scene scene = new Scene(root, 1000, 1000);
+        primStage.setTitle("Den bedste Reversi");
+        Scene scene = new Scene(root, 1000, 800);
         scene.addEventFilter(MouseEvent.MOUSE_PRESSED, this::handleClick);
         scene.setFill(Paint.valueOf("Green"));
+        primStage.setResizable(false);
 
+        player.setX(850);
+        player.setY(200);
+        root.getChildren().add(player);
+
+        primStage.getIcons().add(new Image("file:./img/IconReversi.png"));
+
+        for(int i = 0; i < scene.getHeight()+100; i+=100){
+            Line Xgridline = new Line(i, 0, i, scene.getHeight());
+            Xgridline.setFill(Paint.valueOf("black"));
+            root.getChildren().add(Xgridline);
+
+            Line Ygridline = new Line(0, i, scene.getWidth()-200, i);
+            Ygridline.setFill(Paint.valueOf("black"));
+            root.getChildren().add(Ygridline);
+        }
+        
         primStage.setScene(scene);
         primStage.show();
-
     }
 
-    private void handleClick(MouseEvent event){
-        Circle circle = new Circle();
-        circle.setCenterX(event.getX());
-        circle.setCenterY(event.getY());
-        circle.setRadius(50);
+    /**
+     * 
+     * @param event
+     */
+    private void handleClick(MouseEvent event){        
+        Point p = new Point(makeInt(event.getX()), (makeInt(event.getY())));
+        setCircle(p);
+    }
 
-        //Skifter farven for hvert click
-        if(color == 0){
-            circle.setFill(Paint.valueOf("black"));
-            color = 1;
-        } else if(color == 1){
+    private int makeInt(double dToI){
+        int newInt = (int) Math.round(dToI);
+        return newInt/100;
+    }
+
+
+    private void setCircle(Point p){
+        Circle circle = new Circle();
+        circle.setCenterX(p.x * 100 + 50);
+        circle.setCenterY(p.y * 100 + 50);
+        circle.setRadius(40);
+
+        if(color == 1){
             circle.setFill(Paint.valueOf("white"));
-            color = 0;
+            player.setText("Sort");
+            player.setFill(Paint.valueOf("black"));
+            color = 2;
+        } else if(color == 2){
+            player.setText("Hvid");
+            circle.setFill(Paint.valueOf("black"));
+            player.setFill(Paint.valueOf("white"));
+            color = 1;
         }
-        circles.add(circle);
-        root.getChildren().add(circle);
-        System.out.println(circles.toString());
+
+        
+        if(circle.getCenterX() <= 800){
+            System.out.println(color + ", " + p.x + ", " + p.y);
+            circles.add(circle);
+            root.getChildren().add(circle);
+        }
 
     }
 }
