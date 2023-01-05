0f100a70d673dfeb65ec3e38c1da642f4c1f4b6f
LucasJuel <lucas.juel.sjostrom@gmail.com>
2023-01-05 20:36:52 +0100

    devide Brik to Brik, GameBoard, GameDriver

---
+++ Reversi.V1/src/GameBoard.java
@@ -0,0 +1,64 @@
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.stage.Stage;
+import java.awt.Point;
+
+public class GameBoard {
+
+    private int size;
+    private Group root = new Group();
+    public Scene scene = new Scene(root);
+    int height;
+
+    public GameBoard(int size){
+        this.size = size;
+    }
+
+    public Stage draw(Stage primStage){
+        height = size * 100;
+        primStage.setTitle("Den bedste Reversi");
+
+        primStage.setHeight(height+40);
+        primStage.setWidth(height+17+200);
+
+        primStage.setScene(scene);
+        primStage.setResizable(false);
+        scene.setFill(Paint.valueOf("Green"));
+        primStage.getIcons().add(new Image("file:./img/IconReversi.png"));
+
+        for(int i = 0; i < height+100; i+=100){
+            System.out.println(i);
+            Line Xgridline = new Line(i, 0, i, height);
+            Xgridline.setFill(Paint.valueOf("black"));
+            root.getChildren().add(Xgridline);
+
+            Line Ygridline = new Line(0, i, height, i);
+            Ygridline.setFill(Paint.valueOf("black"));
+            root.getChildren().add(Ygridline);
+        }
+        return primStage;
+    }
+
+    public void /* regler */ restart(){
+
+    }
+
+    public Scene getGMScene(){
+        return this.scene;
+    }
+
+    public Group getRoot(){
+        return this.root;
+    }
+
+    public boolean isOK(Point p){
+        if(p.x * 100 < height){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
