31afbaccd8144fe57dbf75ae0f95f360a2feedc0
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-01-07 13:45:11 +0100

    Add move to ReglerDriver and better comments

--- Reversi.V1/src/ReglerDriver.java
+++ Reversi.V1/src/ReglerDriver.java
@@ -10,36 +10,55 @@ public class ReglerDriver {
         // System.out.println(hej.startmoves(1, 6, 3)+" = false");
         // System.out.println(hej.startmoves(0, 4, 3)+" = false");
 
-        System.out.println(hej.startmoves( 3, 3) + " = true");
-        System.out.println(hej.startmoves(4, 3) + " = true");
-        System.out.println(hej.startmoves(4, 4) + " = true");
-        System.out.println(hej.startmoves(3, 4) + " = true");
+        System.out.println(hej.startMoves(3, 3) + " = true");
+        System.out.println(hej.startMoves(4, 3) + " = true");
+        System.out.println(hej.startMoves(4, 4) + " = true");
+        System.out.println(hej.startMoves(3, 4) + " = true");
 
-        hej.standardmovead(1, 3, 2);
-        hej.standardmovead(2, 2, 4);
-        hej.standardmovead(1, 2, 3);
-        hej.standardmovead(1, 5, 4);
-        hej.standardmovead(1, 6, 4);
+        hej.standardMoveDev(1, 3, 2);
+        hej.standardMoveDev(2, 2, 4);
+        hej.standardMoveDev(1, 2, 3);
+        hej.standardMoveDev(1, 5, 4);
+        hej.standardMoveDev(1, 6, 4);
         // System.out.println(hej.startmoves(1, 4, 3)+ " = false");
         // System.out.println(hej.startmoves(1, 3, 3)+ " = false");
 
         printgame(hej);
 
         // System.out.println(hej.legalmove(2));
-        printMap(hej.legalmove(2));
+        printMap(hej.legalMove(2));
 
-        hej.standardmove(2, 7, 4);
+        hej.standardMove(2, 7, 4);
 
         printgame(hej);
         System.out.println("hej");
-        printMap(hej.legalmove(1));
+        printMap(hej.legalMove(1));
 
-        hej.standardmove(1, 4, 5);
-        //printgame(hej);
+        hej.standardMove(1, 4, 5);
+        move(hej, 2, 0, 2);
+        move(hej, 1, 0, 4);
+        move(hej, 2, 0, 6);
+        System.out.println(hej.winner());
+
+        // System.out.println(hej.legalmove(2));
+
+        //
+
+        // printgame(hej);
+
+        // printMap(hej.legalmove(1));
+
+        // hej.standardmove(1, 4, 5);
+        // printgame(hej);
     }
 
+    /**
+     * Printer hele spillet
+     * 
+     * @param hej objektet Regler som er i brug
+     */
     public static void printgame(Regler hej) {
-        int[][] brat = hej.gameboard();
+        int[][] brat = hej.getGameboard();
         for (int i = 0; i <= 7; i++) {
             for (int j = 0; j <= 7; j++) {
 
@@ -49,6 +68,12 @@ public class ReglerDriver {
         }
     }
 
+    /**
+     * Printer et hashmap af hvilke positioner der er mulige at stille en brik på
+     * efter reglerne
+     * 
+     * @param map Hashmappet med alle Point objekterne
+     */
     public static void printMap(Map<Point, List<Point>> map) {
         for (Map.Entry<Point, List<Point>> entry : map.entrySet()) {
             Point key = entry.getKey();
@@ -59,6 +84,28 @@ public class ReglerDriver {
             }
             System.out.println();
         }
+    }
+
+    /**
+     * Laver et move ud fra farve på spillebrættet og kalder legalMove igen
+     * 
+     * @param hej Objektet Regler
+     * @param f Farven af spilleren der skal flytte
+     * @param x x positionen
+     * @param y y positionen
+     */
+    public static void move(Regler hej, int f, int x, int y) {
+        printMap(hej.legalMove(f));
+        hej.standardMove(f, x, y);
+        if (f == 1) {
+            f = 2;
+        } else {
+            f = 1;
+        }
+
+        System.out.println("ny");
+        printMap(hej.legalMove(f));
+        printgame(hej);
     }
 
 }
