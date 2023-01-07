957274964e79e8d88eb1930710a473121e016443
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-01-08 00:57:49 +0100

    Add comments to Brik

--- Reversi.V1/src/Brik.java
+++ Reversi.V1/src/Brik.java
@@ -3,8 +3,7 @@ import javafx.scene.shape.Circle;
 import java.util.ArrayList;
 import java.awt.Point;
 
-
-public class Brik{
+public class Brik {
 
     static int color = 1;
     ArrayList<Circle> circles = new ArrayList<Circle>();
@@ -12,22 +11,34 @@ public class Brik{
     Regler r;
     boolean checkStart;
 
-    public Brik(Regler r){
+    /**
+     * initialize Brik med objektet regler fra det spil der er i gang
+     * 
+     * @param r Objektet Regler
+     */
+    public Brik(Regler r) {
         this.r = r;
         checkStart = r.start();
     }
 
-    public Circle setBrik(Point p){
+    /**
+     * Sætter en brik på spille brættet
+     * 
+     * @param p et Point objekt med positionen på brættet, fra 0 til 7 hvis
+     *          størrelsen er 8
+     * @return retunere et Circle objekt
+     */
+    public Circle setBrik(Point p) {
         circle = new Circle();
         circle.setCenterX(p.x * 100 + 50);
         circle.setCenterY(p.y * 100 + 50);
         circle.setRadius(40);
         circles.add(circle);
 
-        if(r.getStartPlacement() < 4) {
-            if(r.getStartPlacement() > 2){
+        if (r.getStartPlacement() < 4) {
+            if (r.getStartPlacement() > 2) {
                 circle.setFill(Paint.valueOf("white"));
-            } else{
+            } else {
                 circle.setFill(Paint.valueOf("black"));
             }
         } else {
@@ -36,15 +47,22 @@ public class Brik{
         return circle;
     }
 
-    public int getColorRep(){
+    /**
+     * Retunere farven af brikken der bliver brugt
+     * @return 1 for hvid og 2 for sort
+     */
+    public int getColorRep() {
         return color;
     }
 
-    public void setColor(){
-        if(color == 1){
+    /**
+     * Ændre farven af Cirklen til den modsatte af hvad den er
+     */
+    public void setColor() {
+        if (color == 1) {
             circle.setFill(Paint.valueOf("white"));
             color = 2;
-        } else if(color == 2){
+        } else if (color == 2) {
             circle.setFill(Paint.valueOf("black"));
             color = 1;
         }
