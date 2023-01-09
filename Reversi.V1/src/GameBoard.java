b501192b64c979d5748475d5c127ab946f6d34a3
NinjaKnight1 <christoffer.dam.hansen@gmail.com>
2023-01-09 17:17:47 +0100

    Update GB with last must have

--- Reversi.V1/src/GameBoard.java
+++ Reversi.V1/src/GameBoard.java
@@ -1,21 +1,32 @@
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.awt.Point;
-    
 
 public class GameBoard {
 
     private int size;
-    private Group root = new Group();
-    public Scene scene = new Scene(root);
+    private Group root;
+    public Scene scene;
+    public Button genstart;
     int height;
+    private Text turText;
+    private Text vinderText;
 
     public GameBoard(int size) {
         this.size = size;
+        this.turText = new Text();
+        this.vinderText = new Text();
+        this.genstart= new Button();
+        root = new Group();
+        scene = new Scene(root);
     }
 
     /**
@@ -37,7 +48,6 @@ public class GameBoard {
         primStage.getIcons().add(new Image("file:./img/IconReversi.png"));
 
         for (int i = 0; i < height + 100; i += 100) {
-            System.out.println(i);
             Line Xgridline = new Line(i, 0, i, height);
             Xgridline.setFill(Paint.valueOf("black"));
             root.getChildren().add(Xgridline);
@@ -46,11 +56,54 @@ public class GameBoard {
             Ygridline.setFill(Paint.valueOf("black"));
             root.getChildren().add(Ygridline);
         }
+        setText(turText, 825, 200, 30);
+        setText(vinderText, 825, 400, 30);
+        setButton(genstart, 825, 600, 150, 75, 25, "66, 135, 245", "Spil igen");
+        
+        
         return primStage;
     }
 
-    public void /* regler */ restart() {
+    public void setTurText(int farve) {
+        if (farve == 1) {
+            turText.setFill(Color.WHITE);
+            turText.setText("Hvid");
+        } else {
+            turText.setFill(Color.BLACK);
+            turText.setText("Sort");
+        }
+    }
 
+    public void setVinderText(int farve) {
+        turText.setText(null);
+        if (farve == 1) {
+            vinderText.setFill(Color.WHITE);
+            vinderText.setText("HVID\nVINDER!!");
+        } else if (farve == 2) {
+            vinderText.setFill(Color.BLACK);
+            vinderText.setText("SORT\nVINDER!!");
+        } else {
+            vinderText.setFill(Color.RED);
+            vinderText.setText("UAFGJORT");
+
+        }
+    }
+
+    public void setText(Text text, int x, int y, int fSize) {
+        root.getChildren().add(text);
+        text.setLayoutX(x);
+        text.setLayoutY(y);
+        text.setFont(new Font(fSize));
+    }
+
+    public void setButton(Button but, int x, int y, int xSize, int ySize, int fSize, String rgbString, String text) {
+        root.getChildren().add(but);
+        but.setPrefSize(xSize,ySize);
+        but.setLayoutX(x);
+        but.setLayoutY(y);
+        but.setStyle("-fx-background-color: rgb("+rgbString+"); -fx-font-size: "+fSize+"px; -fx-cursor: hand");
+        but.setTextFill(Color.WHITE);
+        but.setText(text);
     }
 
     public Scene getGMScene() {
@@ -63,6 +116,14 @@ public class GameBoard {
 
     public boolean isOk(Point p) {
         if (p.x * 100 < height) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean knapIsPressed (Point p) {
+        if (genstart.getLayoutX() < p.x && p.x < genstart.getLayoutX()+150 &&genstart.getLayoutY() < p.y && p.y < genstart.getLayoutY()+75) {
             return true;
         } else {
             return false;
